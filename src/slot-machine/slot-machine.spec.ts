dff1c6fdc8fa0dfb3c66963cfe2302aba93b29f8
Roy <roy0983617927@icloud.com>
2026-05-29 18:07:04 +0800

    add balance red

--- src/slot-machine/slot-machine.spec.ts
+++ src/slot-machine/slot-machine.spec.ts
@@ -1,24 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SlotMachine } from './slot-machine';
+import {provideHttpClient} from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('SlotMachine', () => {
   let component: SlotMachine;
   let fixture: ComponentFixture<SlotMachine>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SlotMachine],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SlotMachine);
     component = fixture.componentInstance;
+    httpMock = TestBed.inject(HttpTestingController);
+
+    fixture.detectChanges();
+    httpMock.expectOne('http://localhost:5141/User').flush({
+      name: 'Roy',
+      userMoney: 1000
+    });
     await fixture.whenStable();
   });
+
+  afterEach(() => {
+    httpMock.verify();
+  })
 
   it('SpinAgain', () => {
     expect(component.Name()).toBe('角子老虎機');
     component.ClickSpin();
     expect(component.Name()).toBe('角子老虎機 哪有賭徒天天輸!');
   });
+  it('should show balance', () => {
+    expect(component.Money()).toBe(0);
+  });
+
 });
