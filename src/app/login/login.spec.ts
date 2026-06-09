e03474a4145fec923fb7125570c23080bff94886
ZEROSUM\roy <roy0983617927@icloud.com>
2026-06-09 13:35:56 +0800

    account password clicklogin red

--- src/app/login/login.spec.ts
+++ src/app/login/login.spec.ts
@@ -1,24 +1,38 @@
 ﻿import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {Login} from '/login';
+import {Login} from './login';
+import {provideHttpClient} from '@angular/common/http';
+import {HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { RouterModule } from '@angular/router';
 
 describe('Login', () => {
   let component: Login;
   let fixture: ComponentFixture<Login>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Login],
-      providers: [],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Login);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    httpMock = TestBed.inject(HttpTestingController);
   });
-
-  it('should create', () =>{
-    expect(component).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
   })
 
+  it('按下登入呼叫/Login API', () => {
+    component.Account = 'Roy';
+    component.Password = 'password7777';
+    component.ClickLogin();
+    httpMock.expectOne('http://localhost:5141/login');
+
+  });
 })
